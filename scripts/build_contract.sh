1fdc97260997d84b20ce04bb110b27d16bc625d6
chinhsmartosc <chinhnd@smartosc.com>
2022-12-13 13:39:07 +0700

    Chore: Add sudo on docker run rust_optimizer

--- scripts/build_contract.sh
+++ scripts/build_contract.sh
@@ -21,7 +21,7 @@ if [ $arch == "arm64" ]; then
 fi
 
 # compile contract
-docker run --rm -v "$(pwd)":/code \
+sudo docker run --rm -v "$(pwd)":/code \
   --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
   --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
   $rust_optimizer_image
